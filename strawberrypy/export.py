64a030f9de3dd4afff00a9a2baf5ec93c2a16b13
Patrick <huangpatrick16777216@gmail.com>
2020-10-22 19:13:27 -0700

    Edit: Saves frames to images.

--- strawberrypy/export.py
+++ strawberrypy/export.py
@@ -20,5 +20,10 @@ import numpy
 from PIL import Image
 
 def Export(data, path):
-    tmpDir = os.path.join(os.path.dirname(__file__), "tmp")
+    PARENT = os.path.dirname(__file__)
+    tmpDir = os.path.join(PARENT, "tmp")
     os.makedirs(tmpDir, exist_ok=True)
+
+    for i, frame in enumerate(data.Render()):
+        pixels = numpy.array(frame, dtype=numpy.uint8)
+        Image.fromarray(pixels).save(os.path.join(PARENT, "tmp", f"{i}.jpg"))
